fe8da60700b936971dccafd0982faa52c917a3b4
mrkesseh@gmail.com <mrkesseh@gmail.com>
2023-11-10 12:39:44 +0200

    First commit - committing all files to repo

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+// USER LOGGIN PRCOESS
+
+
+function userLogin(){
+  // const username = "abcd"
+  // const password = "123"
+  
+  // const inputUsername = document.querySelector(".js-username").value
+  // const inputUserPassword = document.querySelector(".js-user-password").value
+  
+  // if(inputUsername === username && inputUserPassword === password){
+  //   console("You have logged in!")
+  //   alert("You have logged in!")
+  // }else {
+  //   alert("Login failed!")
+  //   console.log("Login failed! Try again!")
+  // }
+
+  console.log("Hi")
+}
